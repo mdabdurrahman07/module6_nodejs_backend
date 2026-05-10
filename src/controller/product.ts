d84ff20a480507cb7cc7dfd349ea0714261b0e97
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-05-10 22:00:47 +0600

    Refactor product data structure and enhance product retrieval functionality

--- src/controller/product.ts
+++ src/controller/product.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { readProduct } from "../services/products/readProduct";
+import type { Product } from "../types/productTypes";
 
 export const productController = (
   req: IncomingMessage,
@@ -9,7 +10,13 @@ export const productController = (
   const url = req.url;
   // method
   const method = req.method;
-  if (url === "/product" && method === "GET") {
+
+  const urlParts = url?.split("/");
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
+  // console.log(id);
+
+  if (url === "/products" && method === "GET") {
     // const products = [
     //   {
     //     id: 1,
@@ -27,7 +34,14 @@ export const productController = (
     const products = readProduct();
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
-      JSON.stringify({ message: "This is product route", data: products }),
+      JSON.stringify({ message: "This is products route", data: products }),
+    );
+  } else if (method === "GET" && id !== null) {
+    const products = readProduct();
+    const product = products.find((p: Product) => p.id === id);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({ message: "This is products route", data: product }),
     );
   }
 };
---
+++ src/types/productTypes.ts
@@ -0,0 +1,9 @@
+export interface Product {
+  id: number;
+  name: string;
+  brand: string;
+  category: string;
+  price: number;
+  stock: number;
+  rating: number;
+}
