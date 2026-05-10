81bbab6a5eab7169aabc88d4f7d2771881df0e79
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-05-10 20:50:04 +0600

    Refactor server setup to handle routes and responses for root and product endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
-import { createServer, IncomingMessage, Server } from "http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
-const server: Server = createServer((req: IncomingMessage, res) =>{
-    console.log(req)
-})
+const server: Server = createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    // url
+    const url = req.url;
+    // method
+    const method = req.method;
 
-server.listen(6000,() =>{
-    console.log(`server is running on the 6000 port`)
-})
+    if (url === "/" && method === "GET") {
+      //console.log(`you are on ${url} and method is ${method}`);
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "This is root route" }));
+    } else if (url?.startsWith("/product")) {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "This is product route" }));
+    } else {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "Route not found" }));
+    }
+  },
+);
+
+server.listen(5000, () => {
+  console.log(`server is running on the 5000 port`);
+});
