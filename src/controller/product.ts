4105ff3804f39916388a06c1f91c649fcbb7ca6a
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-05-10 21:12:39 +0600

    Update product route response to return a list of products

--- src/controller/product.ts
+++ src/controller/product.ts
@@ -9,9 +9,23 @@ export const productController = (
   // method
   const method = req.method;
   if (url === "/product" && method === "GET") {
+    const products = [
+      {
+        id: 1,
+        productName: "P-1",
+      },
+      {
+        id: 2,
+        productName: "P-2",
+      },
+      {
+        id: 3,
+        productName: "P-3",
+      },
+    ];
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
-      JSON.stringify({ message: "This is product route", data: { id: 1 } }),
+      JSON.stringify({ message: "This is product route", data: products }),
     );
   }
 };
