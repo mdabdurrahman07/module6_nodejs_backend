4be757a59375d4824ca3dc7c5c2679df6cddde95
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-05-10 21:39:07 +0600

    Refactor product retrieval to read from database file

--- src/controller/product.ts
+++ src/controller/product.ts
@@ -10,21 +10,21 @@ export const productController = (
   // method
   const method = req.method;
   if (url === "/product" && method === "GET") {
-    const products = [
-      {
-        id: 1,
-        productName: "P-1",
-      },
-      {
-        id: 2,
-        productName: "P-2",
-      },
-      {
-        id: 3,
-        productName: "P-3",
-      },
-    ];
-    readProduct();
+    // const products = [
+    //   {
+    //     id: 1,
+    //     productName: "P-1",
+    //   },
+    //   {
+    //     id: 2,
+    //     productName: "P-2",
+    //   },
+    //   {
+    //     id: 3,
+    //     productName: "P-3",
+    //   },
+    // ];
+    const products = readProduct();
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
       JSON.stringify({ message: "This is product route", data: products }),
--- src/services/products/readProduct.ts
+++ src/services/products/readProduct.ts
@@ -1,8 +1,11 @@
-import path from "node:path"
+import path from "node:path";
+import fs from "fs";
+const filePath = path.join(process.cwd(), "./src/dataBase/db.json");
 
-const filePath = path.join(process.cwd(), "./src/dataBase/db.json")
-
-export const readProduct = () =>{
-    // console.log(process.cwd())
-    console.log(filePath)
-}
+export const readProduct = () => {
+  // console.log(process.cwd())
+  // console.log(filePath)
+  const products = fs.readFileSync(filePath, "utf-8");
+  // console.log(products);
+  return JSON.parse(products);
+};
